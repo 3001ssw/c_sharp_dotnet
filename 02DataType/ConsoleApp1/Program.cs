c9877983d1e60dd6339861148b97aee18dce3125
3001ssw <3001ssw@naver.com>
2024-07-24 22:00:05 +0900

    modify data type

--- 02DataType/ConsoleApp1/Program.cs
+++ 02DataType/ConsoleApp1/Program.cs
@@ -22,19 +22,38 @@ Console.WriteLine("f: " + f);
 Console.WriteLine("d: " + d);
 Console.WriteLine("money: " + money);
 
-// 문자형
-
+// 문자형 - 문자
 System.Char ch = 'A';
 Console.WriteLine("ch: " + ch);
 
-System.Char newline = '\n';
-Console.WriteLine(newline);
+System.Char newline = '\n'; // 뉴라인
+Console.Write(newline);
 Console.WriteLine(ch);
 
-System.Char tab = '\t';
-Console.WriteLine(tab);
+System.Char tab = '\t'; // 탭
+Console.Write(tab);
 Console.WriteLine(ch);
 
-System.Char unicode = '\uAEC0';
-Console.WriteLine(unicode);
-Console.WriteLine(ch);
+System.Char escape = '\\'; // escape
+Console.WriteLine(escape);
+
+System.Char unicode = '\uAEC0'; // 유니코드
+Console.WriteLine("U+AEC0: " + unicode);
+
+// 문자형 - 문자열
+System.String text1 = "Hello, World";
+Console.WriteLine(text1);
+
+System.String text2 = "\tHello,\tWorld"; // escape
+Console.WriteLine(text2);
+
+System.String text3 = "\"Hello,World\""; // 쌍따옴표 표시
+Console.WriteLine(text3);
+
+System.String text4 = @"\tHello,\tWorld"; // 그대로 표시
+Console.WriteLine(text4);
+
+
+// boolean
+bool bIsRealNumber = false;
+Console.WriteLine(bIsRealNumber);
