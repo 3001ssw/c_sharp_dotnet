99c73e1cdbfcafba766c97ac56f7d0035fa1eab0
3001ssw <3001ssw@naver.com>
2024-07-30 00:07:28 +0900

    class

---
+++ 03Class/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Movie
+{
+    string name;
+    string author;
+    int year;
+    int month;
+    int day;
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    public string GetName()
+    {
+        return this.name;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Movie movie = new Movie();
+        movie.SetName("titanic");
+        System.Console.WriteLine("name: " + movie.GetName());
+    }
+}
