bc9043a9a012a9e4bea47bc9342a499ba24b1792
3001ssw <3001ssw@naver.com>
2024-07-30 23:55:50 +0900

    class 2

--- 03Class/ConsoleApp1/Program.cs
+++ 03Class/ConsoleApp1/Program.cs
@@ -7,6 +7,25 @@ class Movie
     int month;
     int day;
 
+    // 생성자
+    public Movie()
+    {
+        this.name = "";
+        this.author = "";
+        this.year = 0;
+        this.month = 0;
+        this.day = 0;
+
+        Console.WriteLine("Constructor");
+    }
+    // 소멸자
+    ~Movie()
+    {
+        // Garbage Collector에서 관리하므로 굳이 정의안해도 됨. .NET에서 관리하지 않는 자원인경우만 정의
+        Console.WriteLine("Destructor");
+    }
+
+    // 한정자 반환값 함수명 인자
     public void SetName(string name)
     {
         this.name = name;
---
+++ 03Class/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Person
+{
+    public string _name;
+
+    public Person(string name)
+    {
+        _name = name;
+        Console.WriteLine("public Person 실행(name: " + name +")");
+    }
+
+    static Person()
+    {
+        // 정적 함수가 호출되거나, 객체가 생성되면 1회 실행
+        Console.WriteLine("static Person 실행");
+    }
+
+    public static void WritePerson()
+    {
+        Console.WriteLine("WritePerson");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person.WritePerson();
+        Person person = new Person("hi");
+    }
+}
