03a8e6debb02ede420f0284e2a3c701abf5ca3cf
3001ssw <3001ssw@naver.com>
2024-08-06 23:10:44 +0900

    this, base

---
+++ 04Class/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Array
+class Program
+{
+    // 배열 정보 출력
+    private static void OutputArrayInfo(Array arr)
+    {
+        Console.WriteLine("배열 차원 수: " + arr.Rank);
+        Console.WriteLine("배열 요소 수: " + arr.Length);
+        Console.WriteLine("==================================");
+    }
+
+    //배열 요소 출력
+    private static void OutputArrayElement(Array arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr.GetValue(i) + ", ");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("==================================");
+    }
+    static void Main(string[] args)
+    {
+        bool[,] arrBool = new bool[,] { { true, true }, { false, false } };
+        OutputArrayInfo(arrBool);
+
+        int[,,] arrInt1 = new int[,,] {
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            },
+            {
+                { 10, 11, 12 },
+                { 13, 14, 15 },
+                { 16, 17, 18 }
+            }
+        };
+        OutputArrayInfo(arrInt1);
+
+        int[] arrInt2 = new int[] { 7, 4, 5, 6, 9, 2, 1 };
+        OutputArrayInfo(arrInt2);
+        OutputArrayElement(arrInt2);
+
+        Console.WriteLine("sort");
+        Array.Sort(arrInt2);
+        OutputArrayElement(arrInt2);
+
+        int[] arrInt3 = new int[arrInt2.Length];
+        Array.Copy(arrInt2, arrInt3, arrInt3.Length);
+        OutputArrayInfo(arrInt3);
+        OutputArrayElement(arrInt3);
+    }
+}
---
+++ 04Class/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//this
+class Book
+{
+    string title;
+    string description;
+
+    public Book() : this(string.Empty) { }
+    public Book(string title) : this(title, string.Empty) { }
+    public Book(string title, string description) { this.title = title; this.description = description; }
+
+    public void PrintBookInfo()
+    {
+        Console.WriteLine("title: " + this.title);
+        Console.WriteLine("description: " + this.description);
+    }
+}
+
+// base
+class ElecBook : Book
+{
+    public ElecBook() : base(string.Empty) { }
+    public ElecBook(string title) : base(title, string.Empty) { }
+    public ElecBook(string title, string description) : base(title, description) { }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new Book("C# 프로그래밍");
+        Book book2 = new Book("C# 프로그래밍2", "개정판");
+
+        book1.PrintBookInfo();
+        book2.PrintBookInfo();
+
+        ElecBook book3 = new ElecBook("C# 프로그래밍3", "전자책");
+        ElecBook book4 = new ElecBook("C# 프로그래밍4", "전자책 개정판");
+        book3.PrintBookInfo();
+        book4.PrintBookInfo();
+    }
+}
